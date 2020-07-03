cd67bf7d78dff248a713ad14c75f80443db7c1cf
Bruno <bruno.3av@gmail.com>
2020-07-03 13:54:05 -0300

    bom dia

---
+++ d4.js
@@ -0,0 +1,64 @@
+
+let total = 0
+let quantidade = 0
+let conta = true
+let clicado = false
+
+function roda(num, quant) {
+    if (clicado) {
+        alert('Selecione somar ou subtrair.')
+    } else {
+        let resultado = 0
+
+        quantidade = document.getElementById(quant).value
+        
+        for(let i = 0; i < quantidade; i++) {
+            resultado += Math.floor(Math.random() * num) + 1
+        }
+        
+        if (conta) total += resultado
+        else total -= resultado
+
+        document.getElementById("resultado").value = total
+        document.getElementById("conta").value += resultado
+
+        if(quantidade != '' && quantidade != 1) {
+            document.getElementById("mostraDados").value += `${quantidade}D${num}`
+        } else {
+            document.getElementById("mostraDados").value += `D${num}`
+        }
+        quantidade = ''
+    }
+
+    clicado = true
+
+}
+
+function maisMenos(aux) {
+    conta = aux
+
+    if (aux) {
+        document.getElementById("conta").value += " + "
+        document.getElementById("mostraDados").value += " + "
+    }
+    else {
+        document.getElementById("conta").value += " - "
+        document.getElementById("mostraDados").value += " - "
+    }
+
+    clicado = false
+}
+
+function reset() {
+    total = 0
+
+    document.getElementById("q4").value = 1
+    document.getElementById("q6").value = 1
+
+    document.getElementById("resultado").value = ''
+    document.getElementById("conta").value = ''
+    document.getElementById("mostraDados").value = ''
+
+    clicado = false
+}
+
